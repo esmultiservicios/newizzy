490fdc36d8a4c1f2a9558ea3768d54dbf045f7e1
Edwin Velasquez <edwin.velasquez@clinicarehn.com>
2025-05-06 04:41:29 -0600

    se corrigio error en la carga de las cuentas
    se corrigio error en la carga de las cuentas

--- core/llenarDataTableCuentas.php
+++ core/llenarDataTableCuentas.php
@@ -5,7 +5,7 @@ require_once 'mainModel.php';
 
 $insMainModel = new mainModel();
 
-$estado = isset($_POST['estado']) ? $_POST['estado'] : 1;
+$estado = (isset($_POST['estado']) && $_POST['estado'] !== '') ? $_POST['estado'] : 1;
 
 $result = $insMainModel->getCuentasContabilidad($estado);
 
